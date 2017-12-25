013da901c934e84b3fc466ffb3799ff88aa58460
Laura Luidolt <laura.luidolt@gmail.com>
2017-12-25 12:48:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-  cart[item] = Math.floor(Math.random()*100 + 1);
+  cart.push({
+    [item]:Math.floor(Math.random()*100 + 1)
+  });  
   console.log(`${item} has been added to your cart.`);
   return cart;
 }
